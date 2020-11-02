c67fb3dfe4dbdb4e7b1288b4618ea0293653a39b
yamam00s <s.yamamo0327@gmail.com>
2020-11-02 20:04:56 +0900

    難易度選択ボタンへの切り替え

--- Assets/Scripts/StartGameController.cs
+++ Assets/Scripts/StartGameController.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class StartGameController : MonoBehaviour {
     private AudioSource audioSource;
+    public GameObject selectDifficulty;
+    public GameObject startButton;
 
     void Start () {
         audioSource = GetComponent<AudioSource>();
     }
 
-    void OnClickStartGame() {
+    public　void OnClickStartButton() {
+        audioSource.PlayOneShot(audioSource.clip);
+        startButton.SetActive(false);
+        selectDifficulty.SetActive(true);
+    }
+    public　void OnClickSelectDifficulty() {
         audioSource.PlayOneShot(audioSource.clip);
         SceneManager.LoadScene("Main");
     }

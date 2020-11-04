8e02baf3746413577d3fd9406cdd422879556a5e
yamam00s <s.yamamo0327@gmail.com>
2020-11-05 00:56:18 +0900

    Jsonを読み込み

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonQuizClass {
+    public string top;
+    public string bottom;
+    public string[] answers;
+    public string correct;
+}
+
+[System.Serializable]
+public class MainController : MonoBehaviour {
+    public TextAsset quizCsvJson;
+    // private string[] quizItems;
+
+    // Start is called before the first frame update
+    void Start() {
+        JsonQuizClass inputJsonQuiz = JsonUtility.FromJson<JsonQuizClass>(quizCsvJson.ToString());
+        // Debug.Log(jsonQuizClass.bottom);
+    }
+
+    // // Update is called once per frame
+    // void Update() {
+    // }
+}

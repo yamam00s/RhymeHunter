f3067179bb7dca0957fc30e19a3f711e9a541a4e
yamam00s <s.yamamo0327@gmail.com>
2020-11-05 01:24:36 +0900

    JsonをTextコンポーネントに

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JsonQuizClass {
     public string topLyric;
     public string bottomLyric;
-    public string[] answersLyrics;
+    public string[] answersLyric;
     public string correctLyric;
 }
 
 [System.Serializable]
 public class MainController : MonoBehaviour {
     public TextAsset quizCsvJson;
-    // public Text topLyricText;
-    // public Text bottomLyricText;
-    // private string[] quizItems;
+    public Text topLyricText;
+    public Text bottomLyricText;
+    public Text Answer1;
+    public Text Answer2;
+    public Text Answer3;
+    public Text Answer4;
 
     // Start is called before the first frame update
     void Start() {
         JsonQuizClass inputQuizJson = JsonUtility.FromJson<JsonQuizClass>(quizCsvJson.ToString());
-        // topLyricText.text = inputQuizJson.topLyric;
-        // bottomLyricText.text = inputQuizJson.bottomLyric;
-        // Debug.Log(jsonQuizClass.bottom);
+        topLyricText.text = inputQuizJson.topLyric;
+        bottomLyricText.text = inputQuizJson.bottomLyric;
+        Answer1.text = inputQuizJson.answersLyric[0];
+        Answer2.text = inputQuizJson.answersLyric[1];
+        Answer3.text = inputQuizJson.answersLyric[2];
+        Answer4.text = inputQuizJson.answersLyric[3];
     }
 
     // // Update is called once per frame

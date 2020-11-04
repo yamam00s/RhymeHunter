adf43b649b17b0ba2157065e46fff7c662ea0410
yamam00s <s.yamamo0327@gmail.com>
2020-11-05 01:47:30 +0900

    難易度Headの場合は回転させる

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    void Update() {
+        // 難易度Headの場合は回転させる
+        if (StartGameController.GetDifficulty() == 2) {
+            transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        }
+    }
+}

fd24fd1f06c170c4b7c22047bdc0f7aa6e4ee36a
yamam00s <s.yamamo0327@gmail.com>
2020-11-14 19:36:47 +0900

    Haterを一定時間で動かすよう変更

--- Assets/Scripts/HaterController.cs
+++ Assets/Scripts/HaterController.cs
@@ -8,10 +8,14 @@ using UnityEngine.SceneManagement;
 public class HaterController : MonoBehaviour {
     private GameObject player;
     private MortionController Mortion;
+    private float spanTime;
+    private float currentTime;
 
     void Start() {
         player = GameObject.Find("Player");
         Mortion = GetComponent<MortionController>();
+        spanTime = 3f;
+        currentTime = 0f;
         // 難易度Easyの場合はHaterを表示しない
         //     if (StartGameController.GetDifficulty() == 0) {
         //         gameObject.SetActive(false);
@@ -19,18 +23,24 @@ public class HaterController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void Update() {
         float x = Input.GetAxisRaw("Horizontal");
 		float y = Input.GetAxisRaw("Vertical");
-        if (x == 0 && y == 0) {
-            return;
-        }
         Mortion.MoveDirection(x, y);
-        Vector2 playerPosition = player.transform.position;
-        transform.position = Vector2.MoveTowards(transform.position, playerPosition, 1);
+
+        currentTime += Time.deltaTime;
+        if (currentTime > spanTime) {
+            currentTime = 0f;
+            Vector2 playerPosition = player.transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, playerPosition, 1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collisionInfo) {
+        if (collisionInfo.gameObject.tag == "Wall") {
+            return;
+        }
+
         MainController.isGameClear = false;
         SceneManager.LoadScene("Result");
     }
